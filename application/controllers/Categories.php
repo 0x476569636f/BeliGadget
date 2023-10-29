1c5bc993a89b998f1346a148e0456f22c3d0336d
0x476569636f <darknet1255@gmail.com>
2023-10-29 18:50:55 +0700

    Fix bug, no double category

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -12,8 +12,6 @@ class Categories extends CI_Controller
         parent::__construct();
         //Load Dependencies
         $this->load->model('m_categories');
-        
-
     }
 
     // List all your items
@@ -32,11 +30,16 @@ class Categories extends CI_Controller
     public function add()
     {
         $data = array(
-            'nama_brand'=> $this->input->post('nama_brand'),
+            'nama_brand' => $this->input->post('nama_brand'),
         );
-        $this->m_categories->add($data);
-        $this->session->set_flashdata('pesan', 'Data Berhasil Ditambahkan !');
-        redirect('categories');
+        if ($this->m_categories->is_category_exist($data['nama_brand'])) {
+            $this->session->set_flashdata('error', 'Data Kategori Sudah Ada !');
+            redirect('categories');
+        } else {
+            $this->m_categories->add($data);
+            $this->session->set_flashdata('pesan', 'Data Berhasil Ditambahkan !');
+            redirect('categories');
+        }
     }
 
     //Update one item
