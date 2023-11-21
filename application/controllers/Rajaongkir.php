4c6343a073e1fdea4bfa62a49beda4a0deec16e9
0x476569636f <darknet1255@gmail.com>
2023-11-21 23:06:31 +0700

    Integrasi API Rajaongkir & cek ongkir di halaman cart

--- application/controllers/Rajaongkir.php
+++ application/controllers/Rajaongkir.php
@@ -6,6 +6,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Rajaongkir extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_settings');
+    }
     private $api_key = '9efc95c9adffc30eb74bff28c6accdf8';
 
     public function provinsi()
@@ -40,7 +45,7 @@ class Rajaongkir extends CI_Controller
             $data_provinsi = $array_response['rajaongkir']['results'];
             echo "<option value=''>-- PILIH PROVINSI --</option>";
             foreach ($data_provinsi as $key => $value) {
-                echo "<option value='" . $value['province_id'] . "' id_provinsi='".$value['province_id']."'>" . $value['province'] . "</option>";
+                echo "<option value='" . $value['province_id'] . "," . $value['province'] . "' id_provinsi='" . $value['province_id'] . "'>" . $value['province'] . "</option>";
             }
         }
     }
@@ -51,7 +56,7 @@ class Rajaongkir extends CI_Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.rajaongkir.com/starter/city?province=".$provinsi_terpilih,
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/city?province=" . $provinsi_terpilih,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_SSL_VERIFYHOST => 0,
             CURLOPT_SSL_VERIFYPEER => 0,
@@ -77,7 +82,56 @@ class Rajaongkir extends CI_Controller
             // print_r($array_response['rajaongkir']['results'])
             $data_kota = $array_response['rajaongkir']['results'];
             foreach ($data_kota as $key => $value) {
-                echo "<option value='" . $value['city_id'] . "'>" .$value['type'] ." ". $value['city_name'] . "</option>";
+                echo "<option value='" . $value['city_id'] . "," . $value['city_name'] . "'>" . $value['type'] . " " . $value['city_name'] . "</option>";
+            }
+        }
+    }
+
+
+    public function checkongkir()
+    {
+        $lokasi_toko = $this->m_settings->get_data()->lokasi;
+        $destinasi = $this->input->post('destination');
+        $courier = $this->input->post('courier');
+        $weight = $this->input->post('weight');
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "origin=$lokasi_toko&destination=$destinasi&weight=$weight&courier=$courier",
+            CURLOPT_HTTPHEADER => array(
+                "content-type: application/x-www-form-urlencoded",
+                "key: " . $this->api_key
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $array_response = json_decode($response, true);
+
+            $service = $array_response['rajaongkir']['results'][0]['costs'];
+            
+            if (!empty($array_response['rajaongkir']['results'])) {
+                foreach ($service as $value) {
+                    echo "<input class='form-check-input' type='radio' name='cost' id='value" . $value['service'] . "' value='" . $value['service'] . "' ongkir='" . $value['cost'][0]['value'] . "'>";
+                    echo "<label class='form-check-label font-weight-normal mr-5' for='value" . $value['service'] . "' >" . "(" . $value['service'] . ")" . " " . $value['description'] . " Rp. " . $value['cost'][0]['value'] . "</label>";
+                }
+            } else {
+                echo "<p>Ups Ada Yang Salah, Pastikan Sudah Mengisi Provinsi dan Kota</p>";
             }
         }
     }
--- application/views/customer/v_shopping.php
+++ application/views/customer/v_shopping.php
@@ -40,7 +40,7 @@
 
                                         <br>
                                         <div class="text-right">
-                                            <a href="<?= base_url('shopping/delete/'.$items['rowid']) ?>" class="btn btn-sm btn-danger">
+                                            <a href="<?= base_url('shopping/delete/' . $items['rowid']) ?>" class="btn btn-sm btn-danger">
                                                 <i class="fa fa-trash"></i>
                                             </a>
                                         </div>
@@ -80,7 +80,7 @@
                                 </td>
                                 <td class=" border-0 text-right">&nbsp; : Rp.</td>
                                 <td class=" border-0 text-right">
-                                    <p class="font-weight-bold m-0 h5" align="right">
+                                    <p class="font-weight-bold m-0 h5" id="totalbayar" align="right">
                                         0 </p>
                                 </td>
                             </tr>
@@ -99,7 +99,82 @@
                     <h5><i class="fa fa-user-circle"></i> RINCIAN PENGIRIMAN</h5>
                     <hr>
 
+                    <div class="row">
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="font-weight-bold">NAMA LENGKAP</label>
+                                <input type="text" class="form-control" id="nama_lengkap" placeholder="Nama Lengkap">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="font-weight-bold">NO. HP / WHATSAPP</label>
+                                <input type="number" class="form-control" id="phone" placeholder="No. HP / WhatsApp">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="provinsi" class="font-weight-bold">PROVINSI</label>
+                                <select id="provinsi" name="provinsi" class="form-control">
+
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12" id="hiddenDiv" style="display: none;">
+                            <div class="form-group">
+                                <label for="kota" class="font-weight-bold">KOTA / KABUPATEN</label>
+                                <select id="kota" name="kota" class="form-control">
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="font-weight-bold">KURIR PENGIRIMAN</label>
+                                <br>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input select-courier" type="radio" name="courier" id="ongkos_kirim-jne" value="jne" checked>
+                                    <label class="form-check-label font-weight-bold mr-4" for="ongkos_kirim-jne">
+                                        JNE</label>
+                                    <input class="form-check-input select-courier" type="radio" name="courier" id="ongkos_kirim-tiki" value="tiki">
+                                    <label class="form-check-label font-weight-bold mr-4" for="ongkos_kirim-jnt">TIKI</label>
+                                    <input class="form-check-input select-courier" type="radio" name="courier" id="ongkos_kirim-pos" value="pos">
+                                    <label class="form-check-label font-weight-bold" for="ongkos_kirim-jnt">POS</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <hr>
+                                <label class="font-weight-bold">SERVICE KURIR</label>
+                                <br>
+                                <div class="form-check form-check-inline" id="service">
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="font-weight-bold">ALAMAT LENGKAP</label>
+                                <textarea class="form-control" id="alamat" rows="3" placeholder="Alamat Lengkap&#10;&#10;Contoh: Perum. Griya Palem Indah, B-17 Jombang Jawa Timur 61419"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <button class="btn btn-primary btn-lg btn-block">CHECKOUT</button>
+                        </div>
+
+                    </div>
+
                 </div>
+
 
                 <!-- end ongkos kirim -->
 
@@ -107,3 +182,94 @@
         </div>
     </div>
 </div>
+
+<script src="<?= base_url() ?>template/vendor/jquery/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+
+        // Api Request Provinsi
+        $.ajax({
+            type: "POST",
+            url: "<?= base_url('rajaongkir/provinsi') ?>",
+            success: function(result) {
+                //console.log(result)
+                $("select[name=provinsi]").html(result);
+            }
+        })
+
+        // Api Request Kota
+        $("select[name=provinsi]").on("change", function() {
+            const provinsi_terpilih = $("option:selected", this).attr("id_provinsi");
+
+            $.ajax({
+                type: "POST",
+                url: "<?= base_url('rajaongkir/kota') ?>",
+                data: "id_provinsi=" + provinsi_terpilih,
+                success: function(result_kota) {
+                    //console.log(result_kota)
+                    $("select[name=kota]").html(result_kota);
+                }
+            });
+        })
+
+        $("input[name='courier'], select[name='kota']").on("change", function() {
+
+            var selectedCourier = $("input[name='courier']:checked").val();
+            var destination = $("option:selected", "select[name=kota]").val();
+            var weight = <?= $weight ?>;
+
+            $.ajax({
+                type: "POST",
+                url: "<?= base_url('rajaongkir/checkongkir') ?>",
+                data: 'courier=' + selectedCourier + '&destination=' + destination + '&weight=' + weight,
+                success: function(kurir) {
+                    $("#service").html(kurir)
+                }
+            });
+        })
+    })
+
+    $(document).on("change", "input[name='cost']", function() {
+        var ongkirValue = $(this).attr('ongkir');
+
+        // Check if ongkirValue is a valid number
+        if (!isNaN(ongkirValue)) {
+            // Format ongkirValue
+            var ongkir = new Intl.NumberFormat('id-ID', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            }).format(parseFloat(ongkirValue));
+
+            // Display formatted ongkir
+            $('#ongkir-cart').html(ongkir);
+
+            // Calculate new total
+            var bill = parseFloat(ongkirValue) + parseFloat(<?= $this->cart->total() ?>);
+
+            // Check if bill is a valid number
+            if (!isNaN(bill)) {
+                // Format and display the new total
+                var totalBayar = new Intl.NumberFormat('id-ID', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                }).format(bill);
+                $('#totalbayar').html(totalBayar);
+            } else {
+                console.error("Invalid bill value");
+            }
+        } else {
+            console.error("Invalid ongkirValue");
+        }
+    });
+
+
+    $(document).on("change", "select[name='provinsi']", function() {
+        // Get the selected province
+        var selectedProvince = $(this).val();
+
+        // Toggle the visibility of the hidden div based on whether a province is selected
+        $("#hiddenDiv").toggle(Boolean(selectedProvince));
+    });
+</script>
+
+<i class="fa fa-subscript" aria-hidden="true"></i>
